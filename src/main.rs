47e38a023ccb29e204ad3c7d97bc4c9c777e467f
Chris Davison <c.jr.davison@gmail.com>
2023-03-01 09:26:16 +0000

    move to structopt

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,34 @@
 use anyhow::Result;
-use clap::Parser;
+use structopt::StructOpt;
 
-#[derive(Debug, Parser)]
+#[derive(Debug, StructOpt)]
 /// Suggested tyre pressure (assuming 45:55 load)
-struct Args {
+enum Bike {
+    TyrePressure{
     /// Tyre width, in mm
     width: i32,
     /// Rider weight, in kg
     rider_weight: f32,
     /// Bike weight, in kg
     bike_weight: f32,
+    }
 }
 
 pub fn main() -> Result<()> {
-    let args = Args::parse();
+    let command = Bike::from_args();
 
-    let sysweight = args.rider_weight + args.bike_weight;
-    let wfront = sysweight * 0.45;
-    let wback = sysweight * 0.55;
+    match command {
+        Bike::TyrePressure { width, rider_weight, bike_weight } => {
+            let sysweight = rider_weight + bike_weight;
+            let wfront = sysweight * 0.45;
+            let wback = sysweight * 0.55;
 
-    let wpow: f64 = (args.width as f64).powf(1.5785);
-    let psi_front = ((338.14 * wfront as f64) / wpow) - 7.1685;
-    let psi_back = ((338.14 * wback as f64) / wpow) - 7.1685;
+            let wpow: f64 = (width as f64).powf(1.5785);
+            let psi_front = ((338.14 * wfront as f64) / wpow) - 7.1685;
+            let psi_back = ((338.14 * wback as f64) / wpow) - 7.1685;
 
-    println!("Front: {psi_front:.0} psi\nRear:  {psi_back:.0} psi");
-    Ok(())
+            println!("Front: {psi_front:.0} psi\nRear:  {psi_back:.0} psi");
+        }
+    }
+        Ok(())
 }
